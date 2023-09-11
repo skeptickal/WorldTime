393af288b9f1f21879a39ab9f2853d4eb55a78f0
Jackson Geer <jacksongeer@gmail.com>
2023-09-11 18:42:41 -0400

    added spinner and loader

--- world_time/lib/pages/loading.dart
+++ world_time/lib/pages/loading.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:world_time/services/world_time.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class Loading extends StatefulWidget {
   const Loading({super.key});
@@ -30,7 +31,13 @@ class _LoadingState extends State<Loading> {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: Padding(padding: EdgeInsets.all(50.0), child: Text('loading')),
+      backgroundColor: Colors.blue,
+      body: Center(
+        child: SpinKitCircle(
+          color: Colors.white,
+          size: 50.0,
+        ),
+      ),
     );
   }
 }
