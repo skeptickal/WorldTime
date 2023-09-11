2b09a17ff310da993dca0354ea56e89cacf3a825
Jackson Geer <jacksongeer@gmail.com>
2023-09-11 15:27:51 -0400

    used http API and JSON decoding of the response string to utilize information from the web

--- world_time/lib/pages/choose_location.dart
+++ world_time/lib/pages/choose_location.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:logger/logger.dart';
 
 class ChooseLocation extends StatefulWidget {
   const ChooseLocation({super.key});
@@ -9,26 +8,6 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
-  void getData() async {
-    final logger = Logger();
-    // simulate a network request for a username
-    String username = await Future.delayed(const Duration(seconds: 3), () {
-      return 'yoshi';
-    });
-    // simulate a network request for a bio of username
-    String bio = await Future.delayed(const Duration(seconds: 2), () {
-      return 'egg collector';
-    });
-    print('$username - $bio');
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    getData();
-    print('hey there!');
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
